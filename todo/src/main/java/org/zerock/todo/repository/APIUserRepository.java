6b6f2b524d4e2b659f7af1926ab768ab6ffca336
su1jun <qaxsd530@gmail.com>
2024-07-01 23:50:13 +0900

    remove broken-file

--- todo/src/main/java/org/zerock/todo/repository/APIUserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.todo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.zerock.todo.domain.APIUser;
-
-public interface APIUserRepository extends JpaRepository<APIUser, String> {
-}
